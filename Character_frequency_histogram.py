eeb9a51cdfb9b0d3f2ab349848313703463cc086
ERRATIC-TIGER <61560419+ERRATIC-TIGER@users.noreply.github.com>
2021-07-02 19:32:12 -0400

    First Commit

---
+++ Character_frequency_histogram.py
@@ -0,0 +1,25 @@
+from os import strerror
+
+uin = input("Enter the file location: ")
+try:
+    src = open(uin, 'rt', errors='ignore')
+except IOError as e:
+    print("Cannot Open File: ", strerror(e.errno))
+    exit(e.errno)
+
+try:
+    ln = src.read()
+    aldict = {}
+    for ch in ln:
+        if ch.isalpha():
+            ch = ch.lower()
+            if ch in aldict:
+                aldict[ch] += 1
+            else:
+                aldict[ch] = 1
+    src.close()
+    for key, val in sorted(aldict.items()):
+        print(key, " -> ", val) 
+except Exception as e:
+    print("Cannot Read File: ", e)
+    exit(e)
